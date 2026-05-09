38225cf8df1393f495dc8ab4af2e8437fd18d8c3
Richard Brežák <richard@numtide.com>
2026-05-09 22:06:49 +0200

    Generate `sha1`, `sha512`, and `file_size` from the downloaded mod file

--- mrpack/package.nix
+++ mrpack/package.nix
@@ -8,8 +8,15 @@ let
   data = lib.importTOML ../minecraft.toml;
   minecraft = import ../minecraft.nix { inherit pkgs lib; };
 
+  fetchMod =
+    _name: info:
+    pkgs.fetchurl {
+      url = "https://cdn.modrinth.com/data/${info.modrinth_id}/versions/${info.version_id}/${info.filename}";
+      hash = info.hash;
+    };
+
   # Only mods that run on the client belong in a client modpack
-  clientMods = lib.filterAttrs (_: info: info.client or false) data.mods;
+  clientMods = lib.filterAttrs (name: info: info.client or false) data.mods;
 
   inherit (minecraft) serversDat;
 
@@ -22,12 +29,9 @@ let
       minecraft = data.neoforge.minecraft_version;
       neoforge = data.neoforge.version;
     };
-    files = lib.mapAttrsToList (_: info: {
+    files = lib.mapAttrsToList (name: info: {
+      inherit name;
       path = "mods/${info.filename}";
-      hashes = {
-        sha1 = info.sha1;
-        sha512 = info.sha512;
-      };
       env = {
         client = "required";
         server = if info.server or false then "required" else "unsupported";
@@ -35,19 +39,45 @@ let
       downloads = [
         "https://cdn.modrinth.com/data/${info.modrinth_id}/versions/${info.version_id}/${info.filename}"
       ];
-      fileSize = info.file_size;
     }) clientMods;
   };
 in
 pkgs.runCommand "numcraft.mrpack"
   {
-    nativeBuildInputs = [ pkgs.zip ];
-    passAsFile = [ "index" ];
+    nativeBuildInputs = [ pkgs.zip pkgs.jq ];
+    passAsFile = [ "index" "mods" ];
+    mods = builtins.toJSON (lib.mapAttrs fetchMod clientMods);
     index = modrinthIndex;
   }
   ''
     mkdir -p staging/overrides
+
     cp "$indexPath" staging/modrinth.index.json
+
+    for _mod_name in $(jq --raw-output 'keys | .[]' < "$modsPath") ; do
+      printf 'Processing mod %s\n' "$_mod_name"
+      _mod_file=$(jq --arg mod_name "$_mod_name" --raw-output '.[$mod_name]' < "$modsPath")
+      _sha1=$(( sha1sum | cut -f 1 -d ' ' ) < "$_mod_file")
+      _sha512=$(( sha512sum | cut -f 1 -d ' ' ) < "$_mod_file")
+      _file_size=$( stat --format %s "$_mod_file")
+
+      jq \
+        --arg mod_name "$_mod_name" \
+        --arg sha1 "$_sha1" \
+        --arg sha512 "$_sha512" \
+        --arg file_size "$_file_size" \
+        '
+          ( .files[] | select(.name == $mod_name) ) |= ( . + {
+              "hashes": {
+                "sha1": $sha1,
+                "sha512": $sha512
+              },
+              "fileSize": $file_size | tonumber
+            } | del(.name) )
+        ' < staging/modrinth.index.json > staging/modrinth.index.json.tmp
+      mv staging/modrinth.index.json.tmp staging/modrinth.index.json
+    done
+
     cp ${../icon.png} staging/icon.png
     cp ${serversDat} staging/overrides/servers.dat
     (cd staging && zip -r "$out" .)
